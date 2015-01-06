e6f392449dabdc6dff689550a4f6530be418da0f
Katrina Sanford <blueyed_qt@hotmail.com>
2015-01-05 16:34:01 -0800

    Six different ways to make code from app.js work completed

--- dojo.js
+++ dojo.js
@@ -8,7 +8,32 @@
 // 	console.log(what);
 // }
 
-module.exports.say = function(what) {
-	console.log(what);
-}
+// module.exports.say = function(what) {
+// 	console.log(what);
+// }
 
+// exports.say = (function(what){
+// 	console.log(what);
+// });
+
+// module.exports = function() {
+// 	return {
+// 		say: function(what) {
+// 			console.log(what);
+// 		}
+// 	}
+// }();
+
+// (function() {
+// 	module.exports.say = function(what) {
+// 		console.log(what);
+// 	}
+// }())
+
+(function() {
+	module.exports = {
+		say: function(what) {
+			console.log(what);
+		}
+	}
+}())
